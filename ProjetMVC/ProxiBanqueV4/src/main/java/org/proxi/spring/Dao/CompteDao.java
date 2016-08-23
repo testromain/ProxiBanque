b567d4c756a16d537653629d8017c03861b37eeb
romain <romain@>
2016-08-23 17:42:04 +0200

    Revert "first publish"
    This reverts commit 646a4506ae4ba8c29c9620f18d040c9409208115.

--- ProjetMVC/ProxiBanqueV4/src/main/java/org/proxi/spring/Dao/CompteDao.java
+++
@@ -1,8 +0,0 @@
-package org.proxi.spring.Dao;
-
-import org.proxi.spring.Model.Compte;
-
-
-public interface CompteDao extends ProxiBanqueDao<Compte> {
-	
-}
--- ProjetMVC/ProxiBanqueV4/src/main/java/org/proxi/spring/Dao/CustomerDao.java
+++
@@ -1,7 +0,0 @@
-package org.proxi.spring.Dao;
-
-import org.proxi.spring.Model.Customer;
-
-public interface CustomerDao extends ProxiBanqueDao<Customer>{
-	
-}
